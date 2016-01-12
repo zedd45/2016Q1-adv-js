6426803d80f68a6c97525c55598666810d4466c5
zedd45 <christopher.keen@gmail.com>
2016-01-12 12:00:44 -0500

    add a test for dog

--- 02-inheritence/spec/animal.spec.js
+++ 02-inheritence/spec/animal.spec.js
@@ -4,6 +4,7 @@
 
 const Main = require('../index.js');
 const Animal = Main.Animal;
+const Dog = Main.Dog;
 
 
 // https://github.com/jasmine/jasmine/wiki/Background#enter-jasmine
@@ -28,5 +29,21 @@ describe('Animal', function () {
 
         expect(this.Hippo.calories).toBeGreaterThan(1);
     });
+});
 
+
+describe('Dog', function () {
+
+    beforeEach( function () {
+
+        this.Lassie = new Dog('Lassie');
+        spyOn(this.Lassie, 'bark');
+    });
+
+    it('can bark', function () {
+
+        // be wary of changing your code so that your tests pass! This can be an advantage of TDD.
+        this.Lassie.bark();
+        expect(this.Lassie.bark).toHaveBeenCalled();
+    });
 });
