580b56cc226f075c3769392f2e59d7328d6d8b25
zedd45 <christopher.keen@gmail.com>
2016-02-02 17:23:24 -0500

    change test text for clarity of example.

--- 03-scope/spec/scope.spec.js
+++ 03-scope/spec/scope.spec.js
@@ -5,7 +5,7 @@ let jQuery = ScopeExample.jQuery;
 
 describe('Lexical Scope', function () {
 
-    it('modifying things within a nested block can affect the outer scope', function () {
+    it('nested blocks (inner scope) can read & affect the outer scope', function () {
 
         expect(jQuery.find).toEqual(jasmine.any(Function));
     });
