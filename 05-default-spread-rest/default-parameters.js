a54c4c48cb04c7b51457c0b1128f14e4416f4e0c
zedd45 <christopher.keen@gmail.com>
2016-02-16 17:59:27 -0500

    default parameters

--- 05-default-spread-rest/default-parameters.js
+++ 05-default-spread-rest/default-parameters.js
@@ -3,8 +3,32 @@ module.exports = (function () {
     'use strict';
 
     // show ES5 way here
-    const adaptableFunction = function () {};
+    const oldGreeting = function (name) {
 
-    return { adaptableFunction };
+        let greetingSuffix = arguments[0] === undefined ? 'es5' : name;
+
+        if (typeof greetingSuffix === 'function') {
+            greetingSuffix = greetingSuffix();
+        }
+
+        return 'Hello, ' + greetingSuffix + '!';
+    };
+
+    const greeting = function (name='brave new world') {
+
+        const result = typeof name === 'function' ? name() : name;
+        return 'Hello, ' + result + '!';
+    };
+
+    const complexSequence = function (noun, pluralNoun = noun + 's', action = 'fire blasters') {
+
+        return [pluralNoun, action].join(' ');
+    };
+
+    return {
+        greeting,
+        oldGreeting,
+        complexSequence
+    };
 
 })();
--- 05-default-spread-rest/spec/default-parameters.spec.js
+++ 05-default-spread-rest/spec/default-parameters.spec.js
@@ -1,14 +1,45 @@
 'use strict';
 
-// require something here
-
+const defaults = require('../dist/default-parameters');
+const greeting = defaults.greeting;
+const oldGreeting = defaults.oldGreeting;
+const complexSequence = defaults.complexSequence;
 
 describe('Default Parameters', function () {
 
-    it('uses the default value if one if not supplied');
-    it('overrides the default value with the supplied one');
+    it('uses the default value if one if not supplied', function () {
+
+        expect(greeting()).toEqual('Hello, brave new world!');
+        expect(oldGreeting()).toEqual('Hello, es5!');
+    });
+
+    it('overrides the default value with the supplied one', function () {
+
+        const greetingSuffix = 'your name here';
+        const matcher = /your name here!$/; // ES6 template strings?
+
+        expect(greeting(greetingSuffix)).toMatch(matcher);
+        expect(oldGreeting(greetingSuffix)).toMatch(matcher);
+    });
+
     // https://leanpub.com/understandinges6/read#leanpub-auto-functions-with-default-parameter-values
-    // Be careful when using function calls as default parameter values. If you forget the parentheses, such as second = getValue in the last example, you are passing a reference to the function rather than the result of the function call.
-    it('allows functions to be the default value');
-    it('allows parameters earlier in the function definition to be used in later parameters');
+    // make sure when passing a function you are passing what you intended: a function reference, or the result of a function
+
+    it('allows functions to be the default value', function (){
+
+        const matcher = 'Coruscant!';
+        const myGreeting = () => {
+
+            return 'Coruscant';
+        };
+
+        expect(greeting(myGreeting)).toMatch(matcher);
+        expect(oldGreeting(myGreeting)).toMatch(matcher);
+    });
+
+    it('allows parameters earlier in the function definition to be used in later parameters', function () {
+
+        expect(complexSequence('Stormtrooper')).toEqual('Stormtroopers fire blasters');
+        expect(complexSequence('Star Destroyer', undefined, 'fire Turbolasers')).toEqual('Star Destroyers fire Turbolasers');
+    });
 });
