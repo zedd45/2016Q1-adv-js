f08d18c58d70b9a0880b0bd00b8789f73b58b75e
zedd45 <christopher.keen@gmail.com>
2016-02-22 17:49:29 -0500

    Spread operator

--- 05-default-spread-rest/spec/spead.spec.js
+++ 05-default-spread-rest/spec/spead.spec.js
@@ -1,12 +1,48 @@
 'use strict';
 
-// require something here
+const spread = require('../dist/spread');
 
 
 describe('Spread Operator', function () {
 
-    it('allows an expression to be expanded in places where multiple arguments (for function calls) or multiple elements (for array literals) are expected');
-    it('can replace `apply` in some use cases');
-    it('can assemble multiple arrays inline');
-    it('will convert "array like" values to arrays');
+    beforeEach( function () {
+
+        this.celebrities = spread.celebrities;
+        this.visionaries = spread.visionaries;
+        this.characters = spread.characters;
+    });
+
+    it('allows an expression to be expanded in places where multiple arguments (for function calls) or multiple elements (for array literals) are expected', function () {
+
+        const sortedPeople = spread.visionariesFirst();
+        const actorString = spread.insertionAround().join(' ');
+
+        // why concat? because `Array.prototype.push.apply` is not pure; it operates on the instance passed to it
+        expect(sortedPeople).toEqual(this.visionaries.concat(this.celebrities));
+        expect(actorString).toMatch(new RegExp('^cool people ' + this.celebrities.join(' ') + ' who are actors$', 'i'));
+    });
+
+    it('is a more concise replacement for `apply` in some use cases', function () {
+
+        expect(spread.largestInList()).toEqual(1000);
+        expect(spread.largestInListES5()).toEqual(1000);
+    });
+
+    it('can assemble multiple arrays inline', function () {
+
+        const combinedResult = spread.combineAll();
+
+        expect(combinedResult).toEqual(jasmine.arrayContaining(this.celebrities));
+        expect(combinedResult).toEqual(jasmine.arrayContaining(this.visionaries));
+        expect(combinedResult).toEqual(jasmine.arrayContaining(this.characters));
+        expect(combinedResult).toEqual(jasmine.arrayContaining(['all', 'the', 'things']));
+    });
+
+    // this includes DOM elements / references retrieved by `document.querySelector`
+    it('will convert "array like" values to arrays', function () {
+
+        const arrayOfArgs = spread.convertToArray(1,2,3);
+        expect(arrayOfArgs.map).toEqual(jasmine.any(Function));
+        expect(arrayOfArgs).toEqual(jasmine.arrayContaining([1,2,3]));
+    });
 });
--- 05-default-spread-rest/spread.js
+++ 05-default-spread-rest/spread.js
@@ -2,10 +2,60 @@ module.exports = (function () {
 
     'use strict';
 
-    // const names = {};
-    // const titles = {};
-    const people = [];
 
-    return { people };
+    const visionaries = ['Jon Resig', 'Eran Hammer', 'Dan Abramov'];
+    const celebrities = ['Ryan Reynolds', 'Patrick Stewart', 'Harrison Ford'];
+    const characters = ['Deadpool', 'Captain Picard', 'Han Solo'];
+    const numbers = [404, 42, 1000, 502, 3.14];
+
+
+    const visionariesFirst = () => {
+
+        return [...visionaries, ...celebrities];
+    };
+
+
+    const insertionAround = () => {
+
+        return ['Cool', 'People', ...celebrities, 'who', 'are', 'actors'];
+    };
+
+
+    const largestInListES5 = () => {
+
+        // it is also acceptable to supply `null` as the first parameter here; `this` context is not relevant.
+        return Math.max.apply(Math, numbers);
+    };
+
+
+    const largestInList = () => {
+
+        return Math.max(...numbers);
+    };
+
+
+    const combineAll = () => {
+
+        return ['all', ...visionaries, 'the', ...characters, 'things', ...celebrities];
+    };
+
+
+    // using "fat arrow" here will not send undeclared arguments (e.g. named parameters)
+    const convertToArray = function () {
+
+        return [...arguments];
+    };
+
+    return {
+        visionaries,
+        celebrities,
+        characters,
+        visionariesFirst,
+        insertionAround,
+        largestInListES5,
+        largestInList,
+        combineAll,
+        convertToArray
+    };
 
 })();
