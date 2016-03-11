1b56718d2ab1a9015bf7e4d53812e3868eee6fe1
zedd45 <christopher.keen@gmail.com>
2016-03-10 22:49:18 -0500

    tmp

--- project-scaffolding/gulpfile.js
+++ project-scaffolding/gulpfile.js
@@ -1,13 +1,19 @@
-const Gulp = require('gulp');
 const Babel = require('gulp-babel');
+const Gulp = require('gulp');
+const GulpConcat = require('gulp-concat');
+const Sourcemaps = require('gulp-sourcemaps');
+
 
 Gulp.task('default', () => {
 
     // "Fat Arrow" created an implicit return
 
     Gulp.src('./index.js')
+        .pipe(Sourcemaps.init())
         .pipe(Babel({
             presets: ['es2015']
         }))
+        .pipe(GulpConcat('build.js'))
+        .pipe(Sourcemaps.write('.'))
         .pipe(Gulp.dest('dist'));
 });
